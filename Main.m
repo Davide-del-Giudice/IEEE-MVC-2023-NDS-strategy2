634b02b8fe995153fca64b5eb591acf055aca45d
Davide-del-Giudice <91191187+Davide-del-Giudice@users.noreply.github.com>
2023-03-31 14:58:50 +0200

    Add files via upload

---
+++ Main.m
@@ -0,0 +1,82 @@
+% IEEE VTS Motor Vehicles Challenge 2023
+% University of California, Merced
+% Main Script
+%----------------------------------------------------
+clc;clear;
+% close all;
+addpath(genpath('Reporting'))
+addpath('Functions')
+addpath('Tracks')
+% add the folder to the MATLAB path
+addpath(fullfile(pwd, 'FMIKit-Simulink-3.0'))
+% initialize FMI Kit
+FMIKit.initialize()
+
+%% Initialization
+Ts_solver   = 0.01;                                                         % Solver maximum sampling time
+Ts_sample   = 0.01;                                                         % Sampling of tracks and outputs
+
+% Track selection: For more information on the tracks, please refer to the Reamdme.md file on the "Tracks" folder.
+list        = { 'Track No.  1: FTP75',...
+                'Track No.  2: ECE 15',...
+                'Track No.  3: Artemis Urban',...
+                'Track No.  4: Artemis Rural',...
+                'Track No.  5: ViresRuralRoadDescent1',...
+                'Track No.  6: ViresRuralRoadDescent2',...
+                'Track No.  7: NonOptMunich1',...
+                'Track No.  8: OptMunich1',...
+                'Track No.  9: NonOptMunich2' ...
+                'Track No. 10: OptMunich2'};
+[ind,tf]    = listdlg('PromptString',{'Please select a track',...
+            'Please note: Only one can be selected at a time'},'SelectionMode','single','ListString',list,'ListSize',[300 200]);
+
+N_pre       = 30;                                                           % Preview window size
+
+% Generate inputs to the Simulink model
+[Track,urlReport,urlHTML,urlPublish,FolderName,...
+    preview,refSpeed,refCurv,Tend,T_s] = trackSelect(ind,N_pre);
+
+fprintf('Simulation of the Mission: %s',Track)
+fprintf('\n\nInitializing the Simulink model:\n')
+%% System Parameters
+% The following parameters are not allowed to be changed.
+param.fuelcell.Imax         = 40;                                           % [A] maximum Fuel cell current
+param.fuelcell.SOCmax       = 1;                                            % [-] maximum allowed SoC
+param.fuelcell.SOCmin       = 0.1;                                          % [-] minimum allowed SoC
+
+param.fuelcell.H2_tank_level= 0.15;                                         % [kg] H2 tank initial level value
+
+param.bat.SOCmax            = 0.80;                                         % [-] maximum allowed SoC
+param.bat.SOCmin            = 0.20;                                         % [-] minimum allowed SoC
+param.bat.SOC_delta         = 0.05;                                         % [-] SOC for derating
+param.bat.SOC_init          = 0.65;                                         % [-] vehicle battery start SoC
+
+% safety battery/FC SoC values
+param.fuelcell.SOCmin_stop  = 0.01;                                         % [-] vehicle shutdown if FC SoC become lower than this value
+param.fuelcell.SOCmax_stop  = 1;                                            % [-] vehicle shutdown if FC SoC become higher than this value
+
+param.bat.SOCmin_stop       = 0.01;                                         % [-] vehicle shutdown if battery SoC become lower than this value
+param.bat.SOCmax_stop       = 0.99;                                         % [-] vehicle shutdown if battery SoC become higher than this value
+param.bat.Tmax              = 35;                                           % [deg C] maximum allowed temperature
+
+% Baseline EMA parameters
+param.EMAbaseline.kfc       = 0.8;                                          % fuel cell distribution ratio
+param.EMAbaseline.ktv       = 0.5;                                          % torque vectoring ratio
+param.EMAbaseline.a_ad      = 0.5;                                          % front/rear ratio
+
+%% Main simulation
+%% LOADS A MAT FILE GENERATED FOR THE STRATEGY TO BE USED INSIDE THE EMA
+Map = load('Map.mat');
+%%
+fprintf('Please wait until the simulation is completed...\n\n')
+tic;
+out = sim('testEnergyManagementAlgorithm.slx','StopTime',num2str(Tend));
+sim_time = toc;
+fprintf('Success!\t Total simulation time is: \t %d seconds\n',sim_time)
+fprintf('Please check the auto-generated html report for further evaluation of your designed EMA.\n')
+
+%% Reporting (The following lines must remain unchanged)
+Results;
+options.showCode = false;
+publish(urlReport,options);
+web(urlPublish)
---
+++ Tracks/Readme.md
@@ -0,0 +1,68 @@
+# IEEE VTS Motor Vehicles Challenge 2023: A Multi-physical Benchmark Problem for Next Generation Energy Management Algorithms
+
+Competitors are provided with the following **10 mission profiles** to test their developed EMAs. These tracks divide into the following groups:
+
+### 1. Four Standardized Mission Tracks
+
+The first group of tracks correspond to the standard driving cycles, and are linear motion profiles (with no curvature):
+
+- Track 1: EPA Federal Test Procedure - FTP75     			
+
+- Track 2: European standard driving cycle - ECE15   
+
+- Track 3: European Artemis Urban driving cycle - ArtemisUrban
+
+- Track 4: European Artemis Rural driving cycle - Artemis Rural
+
+
+### 2. Six DLR-Provided Mission Tracks
+
+The second group of mission tracks correspond to the reference mission profiles that have been recorded and provided by DLR test facilites under different driving conditions.
+These tracks contain curvature values as wel as reference speed over the mission profile.
+
+Munich inner-city profiles: optimized or non-optmized tracks.
+
+<img src="../media/MVC_MUCinnenstadt2.jpg" style="width: 6in" />
+Figure: An example of Munich-inner city track.
+
+
+## Reference Mission Profile Figures (Velocity & Curvature)
+### Standardized Tracks
+
+* **Track No. 1: FTP75** | Number of track repetitions: 7 | Track duration: 17318 s
+<img src="../media/track1.png" style="width: 6in" />
+
+
+* **Track No. 2: ECE15** | Number of track repetitions: 25 | Track duration: 19500 s
+<img src="../media/track2.png" style="width: 6in"  />
+
+* **Track No. 3: Artemis Urban** | Number of track repetitions: 17 | Track duration: 16881 s
+<img src="../media/track3.png" style="width: 6in"  />
+
+* **Track No. 4: Artemis Rural** | Number of track repetitions: 1 | Track duration: 1082 s
+<img src="../media/track4.png" style="width: 6in"  />
+
+
+### DLR-Specific Tracks
+* **Track No. 5: Vires Rual Road Descent 1** | Number of track repetitions: 2.36 | Track duration: 1770 s
+<img src="../media/track5.png"/>
+
+* **Track No. 6: Vires Rual Road Descent 2** | Number of track repetitions: 24.4 | Track duration: 3030 s
+<img src="../media/track6.png" style="width:3.3686in;height:3.29562in" />
+
+* **Track No. 7: Non-optimized Munich inner-city #1** | Number of track repetitions: 11 | Track duration: 2750 s
+<img src="../media/track9.png" style="width:3.3686in;height:3.29562in" />
+
+* **Track No. 8: Optimized Munich inner-city #1** | Number of track repetitions: 46 | Track duration: 11500 s
+<img src="../media/track8.png" style="width:3.3686in;height:3.29562in" />
+
+* **Track No. 9: Non-optimized Munich inner-city #2** | Number of track repetitions: 6 | Track duration: 1500 s
+<img src="../media/track7.png" style="width:3.3686in;height:3.29562in" />
+
+* **Track No. 10: Optimized Munich inner-city #2** | Number of track repetitions: 37 | Track duration: 9249 s
+<img src="../media/track10.png" style="width:3.3686in;height:3.29562in" />
+
+**Please note:** Some of these illustrated tracks have been repeated over time for a longer mission time in the actual track files.
+## License
+Copyright © 2022-2023 DLR & UCM. The code is released under the [CC BY-NC 4.0 license](https://creativecommons.org/licenses/by-nc/4.0/legalcode). Link to [short summary of CC BY-NC 4.0 license](https://creativecommons.org/licenses/by-nc/4.0/)
+
